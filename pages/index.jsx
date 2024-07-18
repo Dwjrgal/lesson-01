061b2d6e0c3459d6e30abce0e4f282c360526c95
dwjrgal <dwjrgl651@gmail.com>
2024-07-19 00:13:01 +0800

    hero file

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,7 @@
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 import Section from "@/components/section";
-import Hero from "@/components/section2";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
