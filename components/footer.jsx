4c5847d5de983a5b607274a839f281a266ff694f
dwjrgal <dwjrgl651@gmail.com>
2024-07-19 12:51:23 +0800

    footer section

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,41 +1,52 @@
 const Footer = () => {
   return (
-    <footer>
-      <h4>Ready to launch your next project?</h4>
-      <p>
-        With lots of unique blocks, you can easily build a page without <br />{" "}
-        coding. Build your next landing page.
-      </p>
-      <button>Get started a project</button>
-
-      <h1>Brainwave.io</h1>
-      <p>
-        With lots of unique blocks, you can <br />
-        easily build a page without coding. <br /> Build your next landing page.
-      </p>
-
-      <ul>
-        <p>Company</p>
-        <li>About us</li>
-        <li>Contact us</li>
-        <li>Careers</li>
-        <li>Press</li>
-      </ul>
-      <ul>
-        <p>Product</p>
-        <li>Features</li>
-        <li>Pricing</li>
-        <li>News</li>
-        <li>Help desk</li>
-        <li>Support</li>
-      </ul>
-      <ul>
-        <p>Services</p>
-        <li>Digital marketing</li>
-        <li>Content Writing</li>
-        <li>SEO for Business</li>
-        <li>UI Design</li>
-      </ul>
+    <footer className="bg-slate-900 text-white">
+      <div className="flex justify-between px-20 py-20">
+        <div>
+          <h4 className="font-bold text-2xl pb-3">
+            Ready to launch your next project?
+          </h4>
+          <p className="font-extralight text-gray-400">
+            With lots of unique blocks, you can easily build a page without{" "}
+            <br /> coding. Build your next landing page.
+          </p>
+        </div>
+        <button>Get started a project</button>
+      </div>
+      <div className="flex items-center pb-40">
+        <div className="flex flex-col my-10 px-20">
+          <h1 className="font-bold text-xl mb-10">Brainwave.io</h1>
+          <p className="font-extralight text-gray-400">
+            With lots of unique blocks, you can <br />
+            easily build a page without coding. <br /> Build your next landing
+            page.
+          </p>
+        </div>
+        <div className="flex gap-20">
+          <ul className=" flex flex-col gap-4">
+            <p className="text-gray-400 mb-6">Company</p>
+            <li>About us</li>
+            <li>Contact us</li>
+            <li>Careers</li>
+            <li>Press</li>
+          </ul>
+          <ul className=" flex flex-col gap-4">
+            <p className="text-gray-400 mb-6">Product</p>
+            <li>Features</li>
+            <li>Pricing</li>
+            <li>News</li>
+            <li>Help desk</li>
+            <li>Support</li>
+          </ul>
+          <ul className=" flex flex-col gap-4">
+            <p className="text-gray-400 mb-6">Services</p>
+            <li>Digital marketing</li>
+            <li>Content Writing</li>
+            <li>SEO for Business</li>
+            <li>UI Design</li>
+          </ul>
+        </div>
+      </div>
     </footer>
   );
 };
